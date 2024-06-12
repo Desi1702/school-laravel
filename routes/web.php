49e6a1ced0b4ac581570aab7b2748697eae3ef90
Desi <desi.roubos@student.rocva.nl>
2024-06-12 18:50:12 +0200

    planets

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/planets', function () {
+    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+});
